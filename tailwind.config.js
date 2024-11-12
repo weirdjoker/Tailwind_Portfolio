d1bd0e559f8f936a6cad0033094c1bc05c84f306
weirdjoker <benardmbugua7@gmail.com>
2024-11-12 13:54:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.js",   
+    // Add other paths here if you add more HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
